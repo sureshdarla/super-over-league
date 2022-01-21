e12534a94885f04139715a926e1e787c1701befc
sureshdarla <darlasuresh571@gmail.com>
2022-01-21 06:42:38 +0000

    super over league page

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  <div className="super-over-league-bg ">
+    <h1 className="league-heading">Super Over League</h1>
+    <div className="images-container">
+      <div>
+        <img
+          className="image-sizing"
+          src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        />
+        <img
+          className="image-sizing"
+          src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        />
+      </div>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
